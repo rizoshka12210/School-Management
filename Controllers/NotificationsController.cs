4e729e53675ff772dbc54dbe1ca66b25ee326a74
rizoshka12210 <rizoshka12210@gmail.com>
2026-08-19 10:11:03 +0500

    Clear notification badge when notifications are opened

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -17,6 +17,27 @@ public class NotificationsController : Controller
     public async Task<IActionResult> Index()
     {
         var notifications = await _notificationService.GetForUserAsync(User, 30);
+        var fingerprint = await _notificationService.GetCurrentFingerprintAsync(User);
+
+        if (!string.IsNullOrWhiteSpace(fingerprint))
+        {
+            Response.Cookies.Append(
+                NotificationService.SeenCookieName,
+                fingerprint,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    IsEssential = true,
+                    SameSite = SameSiteMode.Lax,
+                    Secure = Request.IsHttps,
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+                });
+        }
+        else
+        {
+            Response.Cookies.Delete(NotificationService.SeenCookieName);
+        }
+
         return View(notifications);
     }
 }
