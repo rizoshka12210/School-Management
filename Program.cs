1740fb641fb29ab36e0c4e14e6d01c07f2f4d626
rizoshka12210 <rizoshka12210@gmail.com>
2026-08-19 09:56:52 +0500

    Register analytics risk and notification services

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 var connectionString =
     builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException(
@@ -19,7 +18,6 @@ var connectionString =
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
-
 
 builder.Services
     .AddIdentity<ApplicationUser, IdentityRole>(options =>
@@ -29,22 +27,18 @@ builder.Services
         options.Password.RequireUppercase = true;
         options.Password.RequireNonAlphanumeric = true;
         options.Password.RequiredLength = 6;
-
         options.User.RequireUniqueEmail = true;
-
         options.Lockout.MaxFailedAccessAttempts = 5;
         options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
     })
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
-
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Auth/Login";
     options.AccessDeniedPath = "/Auth/AccessDenied";
 });
-
 
 builder.Services.AddLocalization();
 
@@ -69,14 +63,14 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
 });
 
 builder.Services.AddScoped<OwnershipHelper>();
-
 builder.Services.AddScoped<AttendanceService>();
 builder.Services.AddScoped<GradeService>();
 builder.Services.AddScoped<SalaryService>();
 builder.Services.AddScoped<TopicCalendarService>();
+builder.Services.AddScoped<StudentRiskService>();
+builder.Services.AddScoped<NotificationService>();
 
 var app = builder.Build();
-
 
 using (var scope = app.Services.CreateScope())
 {
@@ -89,30 +83,19 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRequestLocalization();
-
 app.UseRouting();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
 
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 
-
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
 
 app.Run();
